1d55d84a03547089d77765cecfb911401e925112
MrNubi <99630193+MrNubi@users.noreply.github.com>
2022-02-15 11:43:43 +0900

    if문 연습

--- app/src/main/java/com/beyond/kotlin_basick_2022215/MainActivity.kt
+++ app/src/main/java/com/beyond/kotlin_basick_2022215/MainActivity.kt
@@ -8,6 +8,9 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var Userage : Any
+
+
 
 
 
@@ -24,7 +27,7 @@ class MainActivity : AppCompatActivity() {
             //로그는 개발자 로직 체크용
 
             Log.d("메인 화면","Log 눌림")
-            Log.wtf("썅", "이게 나오네")
+            Log.wtf("로그 wtf", "이게 나오네")
             Log.e("아", "에러임을 알려주는 에러 로그")
 
         }
@@ -39,7 +42,50 @@ class MainActivity : AppCompatActivity() {
         btnVar.setOnClickListener {
             //변수 연습
 
+            val choi = "최재훈"
+            val d = 10
+
+            Toast.makeText(this, "$choi $d", Toast.LENGTH_SHORT).show()
+
             
+
+        }
+
+        btnCondition.setOnClickListener {
+            //조건문 연습
+
+           val Userage = 10
+
+
+            if(Userage >= 20 ){
+
+                Toast.makeText(this, "당신은 성인이시군요", Toast.LENGTH_SHORT).show()
+
+            }
+
+            else if(Userage >= 17 ){
+
+                Toast.makeText(this, "당신은 고등학생이시군요", Toast.LENGTH_SHORT).show()
+
+            }
+
+            else if(Userage >= 13 ){
+
+                Toast.makeText(this, "당신은 중학생이시군요", Toast.LENGTH_SHORT).show()
+
+            }
+
+            else if(Userage >= 8 ){
+
+                Toast.makeText(this, "당신은 초등학생이시군요", Toast.LENGTH_SHORT).show()
+
+            }
+
+            else{
+
+                Toast.makeText(this, "학교를 못가셨군요", Toast.LENGTH_SHORT).show()
+
+            }
 
         }
 
