8b32b9e02a260377cf0b37e579b39cad04e41480
MrNubi <99630193+MrNubi@users.noreply.github.com>
2022-02-15 09:41:59 +0900

    20222015시작

---
+++ app/src/main/java/com/beyond/kotlin_basick_2022215/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.beyond.kotlin_basick_2022215
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
