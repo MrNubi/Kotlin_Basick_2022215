5aae2d4e2ae24e29c0171f647a2a320303e7e203
MrNubi <99630193+MrNubi@users.noreply.github.com>
2022-02-15 10:15:58 +0900

    kotlin extension 적용 Toast btn, 주석 적용

--- app/src/main/java/com/beyond/kotlin_basick_2022215/MainActivity.kt
+++ app/src/main/java/com/beyond/kotlin_basick_2022215/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,11 +16,12 @@ class MainActivity : AppCompatActivity() {
         //setContentView라는 기능을 수행, R.layout.activity_main을 쓰겟다고
 
 
-        var k = findViewById<Button>(R.id.btnLog)
 
-        k.setOnClickListener {
+        btnLog.setOnClickListener {
+            //kotlin extension 활용, but 폐기예정
+            //중괄호 vs 그냥 괄호
 
-              Log.d("","")
+            Log.d("메인 화면","Log 눌림")
 
         }
 
