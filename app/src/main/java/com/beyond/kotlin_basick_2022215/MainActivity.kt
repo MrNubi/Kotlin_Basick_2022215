a96e46725026901778b5bdf40aee326a638ff86a
MrNubi <99630193+MrNubi@users.noreply.github.com>
2022-02-15 09:55:39 +0900

    버튼추가+구시대의 잔재인 파인드뷰바이이아이디

--- app/src/main/java/com/beyond/kotlin_basick_2022215/MainActivity.kt
+++ app/src/main/java/com/beyond/kotlin_basick_2022215/MainActivity.kt
@@ -2,10 +2,26 @@ package com.beyond.kotlin_basick_2022215
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //setContentView라는 기능을 수행, R.layout.activity_main을 쓰겟다고
+
+
+            var k = findViewById<Button>(R.id.btnLog)
+
+            k.setOnClickListener {
+
+                Log.d("","")
+
+            }
+
     }
 }
